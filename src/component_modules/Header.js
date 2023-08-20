d722db48a1110d9502c24623ab7a400619f600b1
Aswinth K <aswinth24@gmail.com>
2023-08-21 01:01:26 +0530

    small change in anchor tag

--- src/component_modules/Header.js
+++ src/component_modules/Header.js
@@ -39,19 +39,19 @@ function Header() {
                 </button>
               </li>
               <li>
-                <a href="##">Collections</a>
+                <a href="#">Collections</a>
               </li>
               <li>
-                <a href="##">Men</a>
+                <a href="#">Men</a>
               </li>
               <li>
-                <a href="##">Women</a>
+                <a href="#">Women</a>
               </li>
               <li>
-                <a href="##">About</a>
+                <a href="#">About</a>
               </li>
               <li>
-                <a href="##">Contact</a>
+                <a href="#">Contact</a>
               </li>
             </ul>
           </nav>
